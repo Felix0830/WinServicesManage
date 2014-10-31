8af8f9dd41acaf76cdb22cf88a1dd62cd0182f43
Felix0830 <ddjunshuhong@126.com>
2014-11-01 04:18:45 +0800

    upload file

---
+++ WinServiceManage/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WinServiceManage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: 要兼容服务名和服务的显示名称，并且这个从配置文件读取（支持多个）
+            var serviceName = "SNMP Trap";
+            bool isExistService;
+            bool isEnable;
+            WindowsServerManage.isServiceIsExisted(serviceName, out isExistService, out isEnable);
+            if (isExistService)
+            {
+                Console.Write("服务存在");
+                if (isEnable)
+                {
+                    Console.WriteLine("，并已启动");
+                }
+                else
+                {
+                    Console.WriteLine("，但未启动，是否要启动（Y/N）");
+                    IsStartService(serviceName);
+                }
+            }
+            else
+            {
+                Console.WriteLine("没找到对应服务");
+            }
+
+            Console.WriteLine("请按任意键退出！");
+            Console.ReadKey();
+        }
+
+        public static void IsStartService(string serviceName)
+        {
+            string inputKey = string.Empty;
+            inputKey = Console.ReadLine().ToLower();
+            do
+            {
+                if (inputKey.Equals("y") || inputKey.Equals("n"))
+                {
+                    break; //跳出循环
+                }
+
+                Console.WriteLine("输入的指令不正确！请输入 Y/N ");
+                inputKey = Console.ReadLine().ToLower();
+
+            } while (true);
+
+            if (inputKey.Contains("n"))
+            {
+                Console.WriteLine("退出");
+                Thread.Sleep(1000);
+                //退出程序
+                Environment.Exit(0);
+            }
+
+            //启动服务
+            if (WindowsServerManage.StartService(serviceName))
+            {
+                Console.WriteLine("服务启动成功！");
+            }
+            else
+            {
+                Console.WriteLine("服务启动失败！");
+            }
+        }
+
+    }
+}
---
+++ WinServiceManage/WindowsServerManage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceProcess;
+
+namespace WinServiceManage
+{
+    public class WindowsServerManage
+    {
+        /// <summary>
+        /// 检查服务存在的存在性
+        /// DisplayName 是服务的显示名称
+        /// </summary>
+        /// <param name=" NameService ">服务名</param>
+        /// <returns>存在返回 true,否则返回 false;</returns>
+        public static void isServiceIsExisted(string NameService, out bool isExistService, out bool isEnable)
+        {
+            isExistService = false;
+            isEnable = false;
+            ServiceController[] services = ServiceController.GetServices();
+
+            foreach (ServiceController s in services)
+            {
+                if (s.DisplayName.ToLower().Contains(NameService.ToLower()))
+                {
+                    isExistService = true;
+                    if (s.Status == ServiceControllerStatus.Running)
+                    {
+                        isEnable = true;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+               
+            }
+        }
+
+        public static bool StartService(string sName)
+        {
+            try
+            {
+                ServiceController serviceObj = new ServiceController(sName);
+                serviceObj.Start();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
